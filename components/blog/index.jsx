fab452c57921ef543e4515aa789ef66c01acd353
Ade Yusuf <adeyusuf005@gmail.com>
2022-10-07 16:54:57 +0700

    edit: memperbaiki halaman blog

--- components/blog/index.jsx
+++ components/blog/index.jsx
@@ -13,15 +13,36 @@ const Blog = () => {
           <a href="#" className="font-medium text-gray-400">Trending</a>
           <a href="#" className="font-medium text-gray-400">Recent</a>
         </div>
-        <div className="flex lg:overflow-y-hidden overflow-y-auto gap-3 my-5">
-          <div className="w-40 h-40">
-            <img src="../images/design.jpg" alt="gambar" className="object-cover object-center" />
+        <div className="flex lg:overflow-y-hidden overflow-x-auto gap-3 my-5">
+          <div className="bg-white w-60 h-full shrink-0">
+            <img src="../images/design.jpg" alt="gambar" className="object-cover object-center w-60 h-60 shrink-0" />
+            <div className="block my-5">
+              <span className="px-3 py-1 bg-gray-300 text-lg text-gray-800">
+                Nature
+              </span>
+              <h3 className="font-medium mt-2 text-lg text-gray-80">Lorem ipsum dolor sit amet.</h3>
+              <div className="flex items-center gap-3">
+                <span className="w-10 h-10 flex justify-center items-center bg-blue-400 rounded-full">
+                  <img src="../images/icons/user.svg" alt="gambar" />
+                </span>
+                <h3 className="font-medium text-lg text-gray-800">John Doe.</h3>
+              </div>
+            </div>
           </div>
-          <div className="w-40 h-40">
-            <img src="../images/design.jpg" alt="gambar" className="object-cover object-center" />
-          </div>
-          <div className="w-40 h-40">
-            <img src="../images/design.jpg" alt="gambar" className="object-cover object-center" />
+          <div className="bg-white w-60 h-full shrink-0">
+            <img src="../images/art.jpg" alt="gambar" className="object-cover object-center w-60 h-60 shrink-0" />
+            <div className="block my-5">
+              <span className="px-3 py-1 bg-gray-300 text-lg text-gray-800">
+                Nature
+              </span>
+              <h3 className="font-medium mt-2 text-lg text-gray-80">Lorem ipsum dolor sit amet.</h3>
+              <div className="flex items-center gap-3">
+                <span className="w-10 h-10 flex justify-center items-center bg-blue-400 rounded-full">
+                  <img src="../images/icons/user.svg" alt="gambar" />
+                </span>
+                <h3 className="font-medium text-lg text-gray-800">John Doe.</h3>
+              </div>
+            </div>
           </div>
         </div>
         <div className="block lg:mb-0 mb-20">
